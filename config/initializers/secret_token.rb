901ca37c4a30de4d6b423b306a1be49fcd2efdb0
en <kmagnusson63@academic.rrc.ca>
2013-11-29 19:16:49 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LastTimeEStore::Application.config.secret_key_base = '1f16df911fdcf4df1f52e3bc4170f7ca9048cb1d58c635b22a21cce61c770a97d624b752a7cd53b0d5fc2df4153c4d9e6a7891955d15819a5075663844cb8e59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LastTimeEStore::Application.config.session_store :cookie_store, key: '_last_time_e_store_session'
