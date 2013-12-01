cb3ee2fe6269b5def25ea77b4b2cc5e757894d58
en <kmagnusson63@academic.rrc.ca>
2013-12-01 15:50:15 -0600

    Category search added

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -12,7 +12,20 @@ class StoreController < ApplicationController
 
   def search_results
 	@categories = Category.all
+	if params[:category].present?
+  	  @cat = params[:category]
+  	  @products = Product.all.where("category_id = #{params[:category]}")
+  	else
+  	  #@products = Product.category.where("name = '#{params[:category]}'").all
+  	  @products = Product.where("name like '%#{params[:keywords]}%'")
+  	end
+  	@navbar = NavBar.all
+  end
+
+  def list_product_by_category
+  	@categories = Category.all
   	@products = Product.where("name like '%#{params[:keywords]}%'")
   	@navbar = NavBar.all
+
   end
 end
