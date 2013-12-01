f699b59cf57f7df260e970cf0a7293efbee3c2c9
en <kmagnusson63@academic.rrc.ca>
2013-11-30 20:29:11 -0600

    Added image model to activeadmin

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,5 @@
 class Image < ActiveRecord::Base
-  belongs_to :image
+
   belongs_to :product
-  has_many :images
-  validates :filename, :location, :product_id, :presence => true
+  validates :filename, :location, :products_id, :presence => true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :manufacturer
-  has_many :images
+  has_many :image
   validates :name, :price,:sku, :presence =>true
 end
